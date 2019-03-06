725abdf992c0a2720db32ac8932eebf2af7a908a
yukihiro hara <yukihr@gmail.com>
2019-03-06 17:05:03 +0900

    add ja locale

--- lib/date_wrapper/locale/locale.rb
+++ lib/date_wrapper/locale/locale.rb
@@ -1,4 +1,5 @@
 ActiveSupport.on_load(:i18n) do
   I18n.load_path << "#{File.dirname(__FILE__)}/en.yml"
   I18n.load_path << "#{File.dirname(__FILE__)}/da.yml"
+  I18n.load_path << "#{File.dirname(__FILE__)}/ja.yml"
 end
